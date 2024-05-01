c0a091d27533cd5559f4c73c55a8342f8da4e0ce
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2024-05-01 10:24:04 +0200

    Actualizacion de la lista de tareas sin recargar la pagina y estado de "cargando" para evitar sobre cargar el bakcend

--- SupabaseReactProyect/supabase-react/src/pages/Home.js
+++ SupabaseReactProyect/supabase-react/src/pages/Home.js
@@ -2,11 +2,14 @@ import { supabase } from "../supabase/client"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import TaskForm from "../component/TaskForm"
+import { useTasks } from "../context/TaskContext"
+import TaskList from "../component/TaskList"
 
 
 function Home() {
 
     const navigate = useNavigate()
+    const {tasks} = useTasks()
 
     useEffect(() => {
         if (!supabase.auth.getUser()) {
@@ -21,6 +24,7 @@ function Home() {
             <button onClick={() => supabase.auth.signOut()}>Logout</button>
 
             <TaskForm />
+            <TaskList />
         </div>
     )
 }
