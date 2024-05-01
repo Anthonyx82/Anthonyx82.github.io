b2246e7937d75b80272b3768a46fbaed25469365
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2024-05-01 10:25:00 +0200

    Listado de tareas

--- SupabaseReactProyect/supabase-react/src/component/TaskForm.js
+++ SupabaseReactProyect/supabase-react/src/component/TaskForm.js
@@ -1,21 +1,13 @@
 import { useState } from "react";
-import { supabase } from "../supabase/client";
+import { useTasks } from "../context/TaskContext";
 
 function TaskForm() {
     const [taskName, setTaskName] = useState("")
+    const { createTask, adding } = useTasks()
 
-    const handleSubmit = async e => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        try {
-            const user = supabase.auth.getUser()
-            const result = await supabase.from("tasks").insert({
-                name: taskName,
-                userid: (await user).data.user.id,
-            })
-            console.log(result)
-        } catch (error) {
-            console.log(error)
-        }
+        createTask(taskName)
     }
 
     return (
@@ -25,8 +17,8 @@ function TaskForm() {
                     name="taskName"
                     placeholder="añade una tarea"
                     onChange={(e) => setTaskName(e.target.value)} />
-                <button>
-                    Guardar
+                <button disabled={adding}>
+                    {adding ? "Guardando..." : "Guardar"}
                 </button>
             </form>
         </div>
