46401e8c41fe5a22d37c383d7ad52da244322ebe
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2024-05-15 18:52:28 +0200

    Documentacion de lado de supabase terminado en la subida de archivos y buckets

--- js/custom.js
+++ js/custom.js
@@ -2,48 +2,48 @@
 
   "use strict";
 
-    // COLOR MODE
-    $(document).ready(function() {
-      
-      var colorMode = localStorage.getItem('colorMode');
-  
-      if (colorMode === 'dark') {
-          $('body').addClass('dark-mode');
-          $('.color-mode-icon').addClass('active');
-      }
+  // COLOR MODE
+  $(document).ready(function () {
 
-      $('.color-mode').click(function() {
-          $('.color-mode-icon').toggleClass('active');
-          $('body').toggleClass('dark-mode');
-  
-          var currentMode = $('body').hasClass('dark-mode') ? 'dark' : 'light';
-          localStorage.setItem('colorMode', currentMode);
-      });
+    var colorMode = localStorage.getItem('colorMode');
+
+    if (colorMode === 'dark') {
+      $('body').addClass('dark-mode');
+      $('.color-mode-icon').addClass('active');
+    }
+
+    $('.color-mode').click(function () {
+      $('.color-mode-icon').toggleClass('active');
+      $('body').toggleClass('dark-mode');
+
+      var currentMode = $('body').hasClass('dark-mode') ? 'dark' : 'light';
+      localStorage.setItem('colorMode', currentMode);
+    });
   });
 
-    // HEADER
-    $(".navbar").headroom();
+  // HEADER
+  $(".navbar").headroom();
 
-    // PROJECT CAROUSEL
-    $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
+  // PROJECT CAROUSEL
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    nav: true
+  });
 
-    // SMOOTHSCROLL
-    $(function() {
-      $('.nav-link, .custom-btn-link').on('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 49
-        }, 1000);
-        event.preventDefault();
-      });
-    });  
+  // SMOOTHSCROLL
+  $(function () {
+    $('.nav-link, .custom-btn-link').on('click', function (event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 49
+      }, 1000);
+      event.preventDefault();
+    });
+  });
 
-    // TOOLTIP
-    $('.social-links a').tooltip();
+  // TOOLTIP
+  $('.social-links a').tooltip();
 
 })(jQuery);
