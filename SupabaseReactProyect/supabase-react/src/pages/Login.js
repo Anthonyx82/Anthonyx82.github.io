1cea686272962a205b7b56e2d5b42abd001e9166
Anthonyx82 <antoniomartinmanzanares2004@gmail.com>
2024-05-18 14:52:33 +0200

    Cambios y mejoras en el codigo

--- SupabaseReactProyect/supabase-react/src/pages/Login.js
+++ SupabaseReactProyect/supabase-react/src/pages/Login.js
@@ -1,25 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { supabase } from "../supabase/client";
-import { useNavigate } from "react-router-dom";
 
 
 function Login() {
   const [email, setEmail] = useState("");
-  const navigate = useNavigate()
+  const [message, setMessage] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await supabase.auth.signInWithOtp({
+      const { error } = await supabase.auth.signInWithOtp({
         email: email,
       });
+      if (error) throw error;
+      setMessage("Email enviado correctament. Revisa tu bandeja de entrada");
+      setError(null);
     } catch (error) {
-      console.error(error);
+      setMessage(null);
+      setError("Error al enviar el email: " + error.message);
     }
   };
 
   return (
     <div className="row pt-4">
+      <div class="h3 m-3 p-2">Introduce tu email para inciar sesion o registrarte</div>
       <div className="col-md-4 offset-md-4">
         <form onSubmit={handleSubmit} className="card card-body">
           <input
@@ -28,9 +33,12 @@ function Login() {
             placeholder="tuemail@email.com"
             onChange={(e) => setEmail(e.target.value)}
             className="form-control mb-2"
+            required
           />
-          <button className="btn btn-primary">send</button>
+          <button className="btn btn-primary">Send</button>
         </form>
+        {message && <div className="alert alert-success mt-2">{message}</div>}
+        {error && <div className="alert alert-danger mt-2">{error}</div>}
       </div>
     </div>
   );
